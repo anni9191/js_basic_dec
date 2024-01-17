d76faa9108b637d990c38f08ef36172b2003208e
Ann <72548732+anni9191@users.noreply.github.com>
2024-01-17 20:41:41 +0100

    Homework_4 was added

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-// Запитую у користувача перше число
-var number1 = prompt("Please enter first number");
+// Отримуємо три числа від користувача через prompt
+var number1 = parseFloat(prompt("Please enter first number:"));
+var number2 = parseFloat(prompt("Please enter second number:"));
+var number3 = parseFloat(prompt("Please enter third number:"));
 
-// Запитую у користувача друге число
-var number2 = prompt("Please enter second number");
+// Перевірка, чи користувач ввів числа
+if (isNaN(number1) || isNaN(number2) || isNaN(number3)) {
+    alert("Please enter numbers");
+} else {
+    // Показуємо через алерт середнє арифметичне цих чисел
+    var average = (number1 + number2 + number3) / 3;
 
-// Перетворюю введені значення на числа
-number1 = parseFloat(number1);
-number2 = parseFloat(number2);
-
-// Виконання операцій додавання, віднімання, множення та поділу двох чисел та виведення результатів
-var sum = number1 + number2;
-var difference = number1 - number2;
-var product = number1 * number2;
-var quotient = number1 / number2;
-
-// Підготувлюю рядок для виведення
-var resultString = number1 + " + " + number2 + " = " + sum + "\n" +
-                   number1 + " - " + number2 + " = " + difference + "\n" +
-                   number1 + " * " + number2 + " = " + product + "\n" +
-                   number1 + " / " + number2 + " = " + quotient;
-
-// Вивожду повідомлення алертом з результатом
-alert(resultString);
+    // Показуємо результат користувачеві
+    alert("Arithmetic average of entered numbers: " + average);
+}
